3017cce871a0029b0372ccf75400a22893cb77bf
Duver Melo <duvermelo2606@gmail.com>
2023-11-01 15:49:22 -0500

    implementacion de primera parte basica de moviedb

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDb) => Movie(
+      adult: movieDb.adult,
+      backdropPath: (movieDb.backdropPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${movieDb.backdropPath}'
+        : 'https://ih1.redbubble.net/image.533910704.5853/poster,504x498,f8f8f8-pad,600x600,f8f8f8.u3.jpg',
+      genreIds: movieDb.genreIds.map((e) => e.toString()).toList(),
+      id: movieDb.id,
+      originalLanguage: movieDb.originalLanguage,
+      originalTitle: movieDb.originalTitle,
+      overview: movieDb.overview,
+      popularity: movieDb.popularity,
+      posterPath: (movieDb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${movieDb.posterPath}'
+        : 'no-poster',
+      releaseDate: movieDb.releaseDate,
+      title: movieDb.title,
+      video: movieDb.video,
+      voteAverage: movieDb.voteAverage,
+      voteCount: movieDb.voteCount
+    );
+}
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,4 @@
 
 
-export 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
+export 'package:cinemapedia/presentation/screens/screens/home_screen.dart';
 
